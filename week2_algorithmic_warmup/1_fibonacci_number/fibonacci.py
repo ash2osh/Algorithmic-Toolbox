5efd1794fb4170298b3efa9c1aeaf0f42e3e7d6a
ash2osh <orochimaru.ash@gmail.com>
2020-02-11 14:55:38 +0200

    fibonacci solution ??

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,13 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
+    if n <= 1:
         return n
+    a, b = 0, 1
+    for _ in range(0, n - 1):
+        a, b = b, a + b
+    return b
+    # return calc_fib(n - 1) + calc_fib(n - 2)
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
 
 n = int(input())
 print(calc_fib(n))
