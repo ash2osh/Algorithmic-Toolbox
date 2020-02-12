380af0cbe6e6afc7e822b185fd294e97e3b46978
ash2osh <orochimaru.ash@gmail.com>
2020-02-12 08:42:41 +0200

    fibonacci last digit

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -1,19 +1,31 @@
 # Uses python3
 import sys
 
-def get_fibonacci_last_digit_naive(n):
+
+def calc_fib(n):
     if n <= 1:
         return n
 
     previous = 0
-    current  = 1
-
+    current = 1
     for _ in range(n - 1):
         previous, current = current, previous + current
+    return current
 
-    return current % 10
+
+def get_fibonacci_last_digit_naive(n):
+    return calc_fib(n) % 10
+
+
+def get_fibonacci_last_digit(n):
+    x = n % 60
+    return calc_fib(x) % 10
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    # input = sys.stdin.read()
+    # n = int(input)
+    # print(get_fibonacci_last_digit_naive(n))
+    for i in range(0, 999999):
+        if get_fibonacci_last_digit(i) != get_fibonacci_last_digit_naive(i):
+            print("worng answer for :", i)
