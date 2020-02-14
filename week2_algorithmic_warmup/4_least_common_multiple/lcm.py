55203a48b17d54b056b19070d08e268790723480
ash2osh <orochimaru.ash@gmail.com>
2020-02-14 21:23:38 +0200

    solved lcm

--- week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,15 +1,39 @@
 # Uses python3
-import sys
+# import sys
 
 def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
+    for l in range(1, a * b + 1):
         if l % a == 0 and l % b == 0:
             return l
+    return a * b
 
-    return a*b
+
+def gcd(a, b):
+    m = min(a, b)
+    x = max(a, b)
+    mod = x % m
+    # print(mod)
+    if mod == 0:
+        return m
+    return gcd(mod, m)
+
+
+# lcm for 27[3*3*3] 12[3*2*2] = 108[27*2*2] or [27 * 12/gcm(3)]
+def lcm(a, b):
+    x = max(a, b)
+    m = min(a, b)
+    # corner cases
+    if m == 0:
+        return m
+    if x % m == 0:
+        return x
+    # solution
+    g = gcd(a, b)
+    return int(x * m / g)
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
+    # input = sys.stdin.read()
+    a, b = map(int, input().split())
     print(lcm_naive(a, b))
-
+    print(lcm(a, b))
