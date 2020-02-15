dc1e1f52d8f61e9a005262ba9a3db4d0297ebc60
ash2osh <orochimaru.ash@gmail.com>
2020-02-15 20:04:33 +0200

    solved pisano assignment 5

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,6 +1,3 @@
-# Uses python3
-# import sys
-
 def get_fibonacci_huge_naive(n, m):
     if n <= 1:
         return n
@@ -24,19 +21,34 @@ def calc_fib(n):
     return b
 
 
+def try_find_pisano():
+    for m in range(2, 9999):
+        print(m, '-->', find_pisano(m))
+
+
+# 2-3 3-8 4-6 5-20 6-24 7-16 8-12 9-33
+
+def find_pisano(m):
+    prev, current = 0, 1
+    for i in range(1, m ** 2 + 1):
+        prev, current = current, (prev + current) % m
+        if current == 1 and prev == 0:
+            return i
+
+
 def get_fibonacci_huge(n, m):
     # find m period length
-
+    p = find_pisano(m)
     # get fib mod length
-
-    f = calc_fib(n % m)
+    f = calc_fib(n % p)
     print(f)
-    # then mod again by m
+    # then mod  by m
     return f % m
 
 
 if __name__ == '__main__':
     # input = sys.stdin.read();
     n, m = map(int, input().split())
-    print("-->", get_fibonacci_huge_naive(n, m))
+    # try_find_pisano()
+    # print("-->", get_fibonacci_huge_naive(n, m))
     print("-->", get_fibonacci_huge(n, m))
