3c1cd0e5e0598f98af9d35148507d372412a8f3b
ash2osh <orochimaru.ash@gmail.com>
2020-02-16 11:46:37 +0200

    solved ass 8

--- week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -1,13 +1,10 @@
-# Uses python3
-from sys import stdin
-
 def fibonacci_sum_squares_naive(n):
     if n <= 1:
         return n
 
     previous = 0
-    current  = 1
-    sum      = 1
+    current = 1
+    sum = 1
 
     for _ in range(n - 1):
         previous, current = current, previous + current
@@ -15,6 +12,13 @@ def fibonacci_sum_squares_naive(n):
 
     return sum % 10
 
+
+# why not square pisano of 10 = 60 / 60**2 = 360
+def fibonacci_sum_squares(n):
+    return fibonacci_sum_squares_naive(n % 360)
+
+
 if __name__ == '__main__':
-    n = int(stdin.read())
+    n = int(input())
     print(fibonacci_sum_squares_naive(n))
+    print(fibonacci_sum_squares(n))
