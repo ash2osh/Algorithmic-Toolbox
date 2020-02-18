c2812df81b47b802ed573e1c3101d9771c9fad35
ash2osh <orochimaru.ash@gmail.com>
2020-02-18 15:10:29 +0200

    stuck at week 3 ass 3

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,17 +1,39 @@
-# Uses python3
-import sys
-
 def get_optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
+    result = 0.
+    remainingCapacity = capacity
+    prices = []
+    for i in range(0, len(weights)):
+        prices.append(values[i] / weights[i])
 
-    return value
+    for _ in range(0, len(weights)):
+        # get max values and weights
+        maxPrice = max(prices)
+        maxIndex = prices.index(maxPrice)
+        maxVal = values[maxIndex]
+        maxWeight = weights[maxIndex]
+        # remove those from arrays
+        weights.pop(maxIndex)
+        values.pop(maxIndex)
+        prices.pop(maxIndex)
+        # weights.remove(maxWeight)
+        # values.remove(maxVal)
+        if remainingCapacity - maxWeight >= 0:
+            remainingCapacity = remainingCapacity - maxWeight
+            result += maxVal
+        else:
+            result += remainingCapacity / maxWeight * maxVal
+            remainingCapacity = 0
+        if remainingCapacity == 0:
+            break
+
+    return result
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
+    data = list(map(int, input().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
+    # print(n, capacity, values, weights)
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -1,11 +1,35 @@
-# python3
-import sys
-
-
 def compute_min_refills(distance, tank, stops):
     # write your code here
+    refill = 0
+    stops.append(distance)  # adding last stop
+    ok = 0
+    remainingTank = tank
+    currentStop = 0
+    prevRefillStop = 0
+    while len(stops) > 0:
+        if remainingTank > distance:
+            ok = 1
+            break
+        #     remaining fuel at this stop
+        remainingTank = tank - (stops[currentStop] - prevRefillStop)
+        if remainingTank < 0:
+            distance = distance - (stops[currentStop - 1] - prevRefillStop)
+            prevRefillStop = stops[currentStop - 1]
+            #     remove stops prev stops and refill tank
+            del stops[0:currentStop]
+            refill += 1
+            currentStop = 0
+            remainingTank = tank
+        else:
+            currentStop += 1
+
+
+    if ok ==1:
+        return refill
+
     return -1
 
 if __name__ == '__main__':
-    d, m, _, *stops = map(int, sys.stdin.read().split())
+    d, m, _, *stops = map(int, input().split())
+    print(d, m, stops)
     print(compute_min_refills(d, m, stops))
