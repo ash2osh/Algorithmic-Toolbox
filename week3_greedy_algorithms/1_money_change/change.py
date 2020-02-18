596e908f21069085012d1861a0ac248dabe62c86
ash2osh <orochimaru.ash@gmail.com>
2020-02-18 12:08:55 +0200

    week 3 ass 1

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,14 @@
-# Uses python3
-import sys
-
 def get_change(m):
-    #write your code here
-    return m
+    count = 0
+    for i in [10, 5, 1]:
+        x = int(m / i)
+        count += x
+        m = m - x * i
+        if m == 0:
+            break
+    return count
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
