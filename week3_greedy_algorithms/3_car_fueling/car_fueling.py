65edde3e5f3a883e68974b914f4ea01e95d112ce
ash2osh <orochimaru.ash@gmail.com>
2020-02-19 08:26:02 +0200

    solved car fueling with ugly code

--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -1,3 +1,4 @@
+# this code looks but ugly shit!
 def compute_min_refills(distance, tank, stops):
     refill = 0
     stops.append(distance)  # adding last stop
@@ -6,7 +7,7 @@ def compute_min_refills(distance, tank, stops):
     currentStop = 0
     prevRefillStop = 0
     while len(stops) > 0:
-        if remainingTank > distance:
+        if remainingTank >= distance:
             ok = 1
             break
         #     remaining fuel at this stop
@@ -14,10 +15,10 @@ def compute_min_refills(distance, tank, stops):
         if remainingTank < 0 and currentStop == 0:
             break  # IMPOSSIBLE
         if remainingTank <= 0:
+            idx = 0
             if remainingTank == 0:
                 idx = 1
-            else:
-                idx = 0
+
             distance = distance - (stops[currentStop - 1 + idx] - prevRefillStop)
             prevRefillStop = stops[currentStop - 1 + idx]
             #     remove stops prev stops and refill tank
