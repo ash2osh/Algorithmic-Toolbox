c8190e054487f6991d827eb602775ac927df98a1
ash2osh <orochimaru.ash@gmail.com>
2020-02-19 06:01:58 +0200

    still trying

--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -1,5 +1,4 @@
 def compute_min_refills(distance, tank, stops):
-    # write your code here
     refill = 0
     stops.append(distance)  # adding last stop
     ok = 0
@@ -12,22 +11,28 @@ def compute_min_refills(distance, tank, stops):
             break
         #     remaining fuel at this stop
         remainingTank = tank - (stops[currentStop] - prevRefillStop)
-        if remainingTank < 0:
-            distance = distance - (stops[currentStop - 1] - prevRefillStop)
-            prevRefillStop = stops[currentStop - 1]
+        if remainingTank < 0 and currentStop == 0:
+            break  # IMPOSSIBLE
+        if remainingTank <= 0:
+            if remainingTank == 0:
+                idx = 1
+            else:
+                idx = 0
+            distance = distance - (stops[currentStop - 1 + idx] - prevRefillStop)
+            prevRefillStop = stops[currentStop - 1 + idx]
             #     remove stops prev stops and refill tank
-            del stops[0:currentStop]
+            del stops[0:currentStop + idx]
             refill += 1
             currentStop = 0
             remainingTank = tank
         else:
             currentStop += 1
 
-
-    if ok ==1:
+    if ok == 1:
         return refill
 
     return -1
+
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, input().split())
