b7e7cbcdac9f4ec08edeb06f00d999fef6db615f
ash2osh <orochimaru.ash@gmail.com>
2020-02-21 21:07:11 +0200

    ass 5 failed for long input of course :)

--- week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
+++ week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
@@ -1,21 +1,54 @@
-# Uses python3
-import sys
 from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
 
+
+def points_in_segment(points, s):
+    for p in points:
+        if s.start <= p <= s.end:  # current segment intersect with prev point
+            return 1
+    return 0
+
+
 def optimal_points(segments):
     points = []
-    #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+    prevSeg = Segment(0, 0)
+    while len(segments) > 0:
+        if len(segments) == 1:
+            if points_in_segment(points, segments[0]) == 1:
+                break
+            else:
+                points.append(segments[0].end)
+                break
+        for s in segments:
+            if (prevSeg.start == 0 and prevSeg.end == 0) or (prevSeg.start == s.start and prevSeg.end == s.end):
+                prevSeg = s
+                continue
+            if points_in_segment(points, s) == 1:  # current segment intersect with prev point
+                segments.remove(s)
+                if prevSeg.start == s.start and prevSeg.end == s.end:
+                    prevSeg = Segment(0, 0)
+                continue
+            if prevSeg.start <= s.end <= prevSeg.end:  # current segment end between prev segment
+                points.append(s.end)
+                segments.remove(s)
+                continue
+            elif s.start <= prevSeg.end <= s.end:  # prev segment end between current segment
+                points.append(prevSeg.end)
+                segments.remove(prevSeg)
+            prevSeg = s
+
     return points
 
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *data = map(int, input.split())
-    segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+    n = int(input())
+    segments = []
+    for i in range(0, n):
+        s = list(map(int, input().split()))
+        segments.append(Segment(s[0], s[1]))
+    # n, *data = map(int, input().split())
+    # segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
     points = optimal_points(segments)
     print(len(points))
     print(*points)
