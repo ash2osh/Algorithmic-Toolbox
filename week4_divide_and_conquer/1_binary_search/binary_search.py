57fee90e8fb4acb5f73e36fa478b7223c88dd3ff
ash2osh <orochimaru.ash@gmail.com>
2020-02-23 15:03:31 +0200

    still at binary zeft

--- week4_divide_and_conquer/1_binary_search/binary_search.py
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,9 +1,26 @@
-# Uses python3
-import sys
+def binary_search_recursive(keys, mn, mx, query):
+    md = int((mx - mn) / 2)
+    if mx - mn <= 1:
+        if query == keys[mn]:
+            return mn
+        elif query == keys[mx]:
+            return mx
+        else:
+            return -1
+    if query == keys[md]:
+        return md
+    elif query > keys[md]:
+        return binary_search_recursive(keys, md + 1, mx, query)
+    elif query < keys[md]:
+        return binary_search_recursive(keys, mn, md - 1, query)
+    return -1
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+
+def binary_search(keys, query):
+    mn, mx = 0, len(keys)-1
+    while 1:
+        md =
+
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -11,12 +28,24 @@ def linear_search(a, x):
             return i
     return -1
 
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    m = data[n + 1]
-    a = data[1 : n + 1]
-    for x in data[n + 2:]:
-        # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+    input_keys = list(map(int, input().split()))[1:]
+    input_queries = list(map(int, input().split()))[1:]
+    mn, mx = 0, len(input_keys) - 1
+    for q in input_queries:
+        print(linear_search(input_keys, q), end=' ')
+
+    print()
+    for q in input_queries:
+        print(binary_search_recursive(input_keys, mn, mx, q), end=' ')
+
+    # print()
+    # for q in input_queries:
+    #     print(binary_search(input_keys, q), end=' ')
+# input
+#
+
+# 5 1 2 3 4 5
+# out
+# 0 1 2 3 4
