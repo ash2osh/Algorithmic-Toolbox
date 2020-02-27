f9c459a6a5d3032fdf3cef796ec2713dd642aec8
ash2osh <orochimaru.ash@gmail.com>
2020-02-27 13:06:40 +0200

    finished week5 ass1 finished recursive ass2

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,19 +1,50 @@
+# dynamic programing solution
 def get_change(money):
-    # write your code here
-    return money // 4
+    if money == 0:
+        return 0
+    elif money < 0:
+        return -1
+    minChange = 0
+    min_changes = []
+    for i in [1, 3, 4]:
+        next_money = money - i
+        if next_money == 0:  # path ends here
+            min_changes.append(1)
+        elif next_money > 0:
+            x = 1
+            if next_money in db:
+                x += db[next_money]
+            else:
+                x += get_change(next_money)
+            if x > 0:  # remove negative paths
+                min_changes.append(x)
+    if len(min_changes) > 0:
+        minChange += min(min_changes)
+        db[money] = minChange
+    return minChange
 
 
 def get_change_rec(money):
-    min_coin = 0
-    # num1, num2, num3 = 0, 0, 0
-    # num1, num2, num3 = {1: 0, 3: 0, 4: 0}, {1: 0, 3: 0, 4: 0}, {1: 0, 3: 0, 4: 0}
-    num1 = 0
-    if money <= 0:
+    if money == 0:
         return 0
-    num1 += 1
-    money = money - 1
-    num1 += get_change_rec(money)
-    return num1
+    elif money < 0:
+        return -1
+    minChange = 0
+    changes = []
+    for i in [1, 3, 4]:
+        next_money = money - i
+        if next_money == 0:  # path ends here
+            changes.append(1)
+        elif next_money > 0:
+            x = 1
+            x += get_change_rec(next_money)
+            if x > 0:  # remove negative paths
+                changes.append(x)
+    if len(changes) > 0:
+        minChange += min(changes)
+        # z = minChange # for debug only
+
+    return minChange
 
 
 def get_change_greedy(money):
@@ -40,8 +71,13 @@ def change_naive(money):
 
 
 if __name__ == '__main__':
-    m = int(input())
-    print(get_change(m))
-    print(get_change_greedy(m))
-    print(change_naive(m))
-    print(get_change_rec(m))
+    n = int(input())
+    db = {}
+    print(get_change(n))
+    # for m in range(0, n + 1):
+    #     print(get_change_greedy(m))
+    #     print(get_change(m))
+    #     print(change_naive(m))
+    #     print(get_change_rec(m))
+    #     print(get_change(m))
+    #     print()
--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,21 +1,53 @@
-# Uses python3
-import sys
+def optimal_sequence_rec(n):
+    operations = []
+    minOperations = []
+    if n == 1:
+        return [1]
+    elif n <= 0:
+        return [-1]
 
-def optimal_sequence(n):
-    sequence = []
+    for i in [1, 2, 3]:
+        x = []
+        if i == 1:
+            next_n = n - 1
+        elif n % i == 0:
+            next_n = n // i
+        else:
+            next_n = n - 1
+        if next_n == 1:
+            x.append(n)
+            x.append(1)
+        elif next_n < n:
+            x.append(n)
+            x.extend(optimal_sequence(next_n))
+        if -1 not in x:  # dos not contain -1
+            operations.append(x)
+    if len(operations) > 0:
+        minOperations += min(operations, key=len)  # https://stackoverflow.com/questions/18741633
+    return minOperations
+
+
+def optimal_sequence_greedy(n):
+    sq = []
     while n >= 1:
-        sequence.append(n)
+        sq.append(n)
         if n % 3 == 0:
             n = n // 3
         elif n % 2 == 0:
             n = n // 2
         else:
             n = n - 1
-    return reversed(sequence)
+    return reversed(sq)
 
-input = sys.stdin.read()
-n = int(input)
-sequence = list(optimal_sequence(n))
+
+def print_array(array):
+    for x in array:
+        print(x, end=' ')
+
+
+input_n = int(input())
+sequence = list(optimal_sequence_greedy(input_n))
 print(len(sequence) - 1)
-for x in sequence:
-    print(x, end=' ')
+print_array(sequence)
+print()
+print_array(reversed(optimal_sequence_rec(input_n)))
