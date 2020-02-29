4d5e398aea42b0f67e8616338ce6181012806b53
ash2osh <orochimaru.ash@gmail.com>
2020-02-29 11:37:27 +0200

    week 5 ass 3 done

--- week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,24 @@
-# Uses python3
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    db[(0, 0)] = 0
+    ls = len(s)
+    lt = len(t)
+    for i in range(1, ls + 1):
+        db[(i, 0)] = i
+    for j in range(1, lt + 1):
+        db[(0, j)] = j
+
+    for i in range(1, ls + 1):
+        for j in range(1, lt + 1):
+            x = [db[(i, j - 1)] + 1, db[(i - 1, j)] + 1]
+            if s[i - 1] == t[j - 1]:
+                x.append(db[(i - 1, j - 1)])
+            else:
+                x.append(db[(i - 1, j - 1)] + 1)
+            db[(i, j)] = min(x)
+
+    return db[(ls, lt)]
+
 
 if __name__ == "__main__":
+    db = {}
     print(edit_distance(input(), input()))
